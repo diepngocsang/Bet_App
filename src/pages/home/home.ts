c0487416a1281da67480ecbd89429cd13207c421
Diep Ngoc Sang <sdiep2@csc.com>
2017-09-28 15:45:42 +0700

    Implementing Login Form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Test, Person} from '../../services/test';
+import { Login } from '../login/login';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  
+  loginPage = Login;
+  items: any[]|Person[];
   constructor(public navCtrl: NavController) {
+    this.initData();
+  }
+  
+  initData(){
+    var a : Test = new Test();
+    this.items=a.getInfo();
+    console.log(this.items);
+  }
 
+  goToLogin(){
+    this.navCtrl.push(Login);
   }
 
 }
---
+++ src/services/test.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+export class Test {
+    public getInfo():Person[]{
+      return [{
+        name: "ABC",
+        age: 18      
+      },{
+        name: "DEF",
+        age: 19
+      },{
+        name: "GHI",
+        age: 20
+      }];
+    }
+}
+
+export class Person {
+  name: String;
+  age: Number;
+}
