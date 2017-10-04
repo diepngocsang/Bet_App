a0e3c16d1dff73b8dfe43806dd1c36fa3cc9ef3e
Diep Ngoc Sang <sdiep2@csc.com>
2017-10-04 16:07:05 +0700

    update test bind data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {Test, Person} from '../../services/test';
+// import {Test, Person} from '../../services/test';
 import { Login } from '../login/login';
 
 @Component({
@@ -10,15 +10,15 @@ import { Login } from '../login/login';
 export class HomePage {
   
   loginPage = Login;
-  items: any[]|Person[];
+  // items: any[]|Person[];
   constructor(public navCtrl: NavController) {
     this.initData();
   }
   
   initData(){
-    var a : Test = new Test();
-    this.items=a.getInfo();
-    console.log(this.items);
+    // var a : Test = new Test();
+    // this.items=a.getInfo();
+    // console.log(this.items);
   }
 
   goToLogin(){
